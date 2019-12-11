d69341f5b0a9fd8032e3411c98aee6c9c078e54c
Sean Biller <seanmbiller@gmail.com>
2019-12-11 09:04:01 -0500

    7 Tests Passing one Extra Credit Failing

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,33 @@
-function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+
+const reconcileincomingOrder = (existingBook, incomingOrder) => {
+  let updatedBook = []
+  
+  if (existingBook.length == 0) {
+    return updatedBook = existingBook.concat(incomingOrder)
+  }
+  if (existingBook[0].type === incomingOrder.type) {
+    return updatedBook = existingBook.concat(incomingOrder)
+  }
+  if (existingBook[0].type !== incomingOrder.type && existingBook[0].price !== incomingOrder.price) { 
+    return updatedBook = existingBook.concat(incomingOrder)
+  }
+
+  if (existingBook[0].type !== incomingOrder.type && existingBook[0].quantity === incomingOrder.quantity && existingBook[0].price === incomingOrder.price) {
+    updatedBook = existingBook.concat(incomingOrder)
+    updatedBook.splice(0, 2)
+    return updatedBook
+  }
+  if (existingBook[0].type !== incomingOrder.type && existingBook[0].quantity > incomingOrder.quantity) {
+    incomingOrder.quantity = existingBook[0].quantity - incomingOrder.quantity
+    incomingOrder.type = existingBook[0].type
+    return updatedBook = updatedBook.concat(incomingOrder)
+  }
+  if (existingBook[0].type !== incomingOrder.type && existingBook[0].quantity < incomingOrder.quantity) {
+    incomingOrder.quantity = incomingOrder.quantity - existingBook[0].quantity
+    return updatedBook = updatedBook.concat(incomingOrder)
+  }
 }
 
-module.exports = reconcileOrder
+
+
+module.exports = reconcileincomingOrder
--- tests.js
+++ tests.js
@@ -64,7 +64,7 @@ describe('Order Book', () => {
       expect(updatedBook).to.deep.equal([{ type: 'sell', quantity: 5, price: 6150 }])
     })
 
-    it.skip('Extra Credit: it fulfills a mismatched order when both parties benefit', () => {
+    it('Extra Credit: it fulfills a mismatched order when both parties benefit', () => {
       const existingBook = [{ type: 'buy', quantity: 15, price: 6000 }]
       const incomingOrder = { type: 'sell', quantity: 15, price: 5900 }
 
@@ -73,7 +73,7 @@ describe('Order Book', () => {
       expect(updatedBook).to.deep.equal([])
     })
 
-    it.skip('Extra Credit: it does not fulfill a mismatched order when it does not benefit both parties', () => {
+    it('Extra Credit: it does not fulfill a mismatched order when it does not benefit both parties', () => {
       const existingBook = [{ type: 'buy', quantity: 15, price: 5900 }]
       const incomingOrder = { type: 'sell', quantity: 15, price: 6000 }
 
